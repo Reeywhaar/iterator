4707cc05a63dfc3937737f186e57837256405d5b
Mikhail Vyrtsev <reeywhaar@gmail.com>
2018-02-12 20:14:16 +0300

    prettier format

--- iterator.js
+++ iterator.js
@@ -3,17 +3,17 @@
  * @constructor
  * @param {iterator} gen - iterator
  */
-function Iterator(gen){
+function Iterator(gen) {
 	this.gen = gen;
 }
 
-Iterator.prototype[Symbol.iterator] = function*(){
+Iterator.prototype[Symbol.iterator] = function*() {
 	yield* this.gen;
-}
+};
 
-Iterator.prototype.next = function(value){
+Iterator.prototype.next = function(value) {
 	return this.gen.next(value);
-}
+};
 
 /**
  * Transform iterator to array.
@@ -21,13 +21,13 @@ Iterator.prototype.next = function(value){
  *
  * @returns {Any[]}
  */
-Iterator.prototype.toArray = function(){
+Iterator.prototype.toArray = function() {
 	let arr = [];
-	for(let item of this.gen){
+	for (let item of this.gen) {
 		arr.push(item);
 	}
 	return arr;
-}
+};
 
 /**
  * function which Iterator instance mepthod consumes
@@ -46,16 +46,16 @@ Iterator.prototype.toArray = function(){
  * @example
  * range(0,5).map(x => x*2) // [0,2,4,6,8]
  */
-Iterator.prototype.map = function(fn){
-	const it = function* () {
+Iterator.prototype.map = function(fn) {
+	const it = function*() {
 		let i = 0;
-		for(let item of this.gen){
+		for (let item of this.gen) {
 			yield fn(item, i);
 			i++;
 		}
-	}
+	};
 	return new Iterator(it.call(this));
-}
+};
 
 /**
  * Executes function over iterator item.
@@ -68,13 +68,13 @@ Iterator.prototype.map = function(fn){
  * range(0,5).forEach(x => arr.push(x*2))
  * arr; // [0,2,4,6,8]
  */
-Iterator.prototype.forEach = function(fn){
+Iterator.prototype.forEach = function(fn) {
 	let i = 0;
-	for(let item of this.gen){
+	for (let item of this.gen) {
 		fn(item, i);
 		i++;
 	}
-}
+};
 
 /**
  * function which Iterator instance mepthod consumes
@@ -93,17 +93,16 @@ Iterator.prototype.forEach = function(fn){
  * @example
  * range(0,5).filter(x => x%2 === 0) // [0,2,4]
  */
-Iterator.prototype.filter = function(fn){
-	const it = function* () {
+Iterator.prototype.filter = function(fn) {
+	const it = function*() {
 		let i = 0;
-		for(let item of this.gen){
-			if(fn(item, i)) yield item;
+		for (let item of this.gen) {
+			if (fn(item, i)) yield item;
 			i++;
 		}
-	}
+	};
 	return new Iterator(it.call(this));
-}
-
+};
 
 /**
  * function which Iterator instance method consumes
@@ -124,23 +123,23 @@ Iterator.prototype.filter = function(fn){
  * @example
  * range(0,5).reduce((c,x) => c+x) // 10
  */
-Iterator.prototype.reduce = function(fn, initial){
+Iterator.prototype.reduce = function(fn, initial) {
 	let obj = initial;
 	let i = 0;
-	if(typeof obj === "undefined"){
+	if (typeof obj === "undefined") {
 		const val = this.gen.next();
 		i++;
-		if(val.done){
+		if (val.done) {
 			throw new Error("reduce of empty iterator with no initial value");
 		}
 		obj = val.value;
-	};
-	for(let item of this.gen){
+	}
+	for (let item of this.gen) {
 		obj = fn(obj, item, i);
 		i++;
-	};
+	}
 	return obj;
-}
+};
 
 /**
  * Enumerate iterator
@@ -149,16 +148,16 @@ Iterator.prototype.reduce = function(fn, initial){
  * @example
  * range(1,5).enumerate() // [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5]]
  */
-Iterator.prototype.enumerate = function(){
-	const it = function* () {
+Iterator.prototype.enumerate = function() {
+	const it = function*() {
 		let i = 0;
-		for(let item of this.gen){
+		for (let item of this.gen) {
 			yield [i, item];
 			i++;
 		}
-	}
+	};
 	return new Iterator(it.call(this));
-}
+};
 
 /**
  * Take first n items of iterator
@@ -168,9 +167,9 @@ Iterator.prototype.enumerate = function(){
  * @example
  * range(0,5).take(2) // [0,1]
  */
-Iterator.prototype.take = function(count){
-	return this.while((x,i) => i < count);
-}
+Iterator.prototype.take = function(count) {
+	return this.while((x, i) => i < count);
+};
 
 /**
  * Skip first n items of iterator
@@ -180,9 +179,9 @@ Iterator.prototype.take = function(count){
  * @example
  * range(0,5).skip(2) // [2,3,4]
  */
-Iterator.prototype.skip = function(count){
-	return this.filter((x,i) => i >= count);
-}
+Iterator.prototype.skip = function(count) {
+	return this.filter((x, i) => i >= count);
+};
 
 /**
  * Yield while while is true
@@ -192,17 +191,17 @@ Iterator.prototype.skip = function(count){
  * @example
  * range(0,5).while(x => x < 3) // [0,1,2]
  */
-Iterator.prototype.while = function(fn){
-	const it = function* () {
+Iterator.prototype.while = function(fn) {
+	const it = function*() {
 		let i = 0;
-		for(let item of this.gen){
-			if(!fn(item, i)) break;
+		for (let item of this.gen) {
+			if (!fn(item, i)) break;
 			yield item;
 			i++;
 		}
-	}
+	};
 	return new Iterator(it.call(this));
-}
+};
 
 /**
  * Takes every nth items of iterator
@@ -212,9 +211,9 @@ Iterator.prototype.while = function(fn){
  * @example
  * range(0,5).everyNth(2) // [0,2,4]
  */
-Iterator.prototype.everyNth = function(n){
-	return this.filter((x, i) => i%n === 0);
-}
+Iterator.prototype.everyNth = function(n) {
+	return this.filter((x, i) => i % n === 0);
+};
 
 /**
  * Take even items of iterator
@@ -223,9 +222,9 @@ Iterator.prototype.everyNth = function(n){
  * @example
  * range(0,5).even() // [0,2,4]
  */
-Iterator.prototype.even = function(){
+Iterator.prototype.even = function() {
 	return this.everyNth(2);
-}
+};
 
 /**
  * Take odd items of iterator
@@ -235,9 +234,9 @@ Iterator.prototype.even = function(){
  * @example
  * range(0,5).odd() // [1,3,5]
  */
-Iterator.prototype.odd = function(){
+Iterator.prototype.odd = function() {
 	return this.skip(1).everyNth(2);
-}
+};
 
 /**
  * Concat multiple iterators
@@ -248,13 +247,13 @@ Iterator.prototype.odd = function(){
  * Iterator.fromArray([0,1,2])
  * .concat(Iterator.fromArray(["a","b","c"])) // [[0,1,2,"a","b","c"]]
  */
-Iterator.prototype.concat = function(...iters){
-	const it = function*(){
+Iterator.prototype.concat = function(...iters) {
+	const it = function*() {
 		yield* this.gen;
-		for(let iter of iters) yield* iter;
-	}
+		for (let iter of iters) yield* iter;
+	};
 	return new Iterator(it.call(this));
-}
+};
 
 /**
  * Prepend multiple iterators
@@ -265,13 +264,13 @@ Iterator.prototype.concat = function(...iters){
  * Iterator.fromArray([0,1,2])
  * .concatLeft(Iterator.fromArray(["a","b","c"])) // [["a","b","c",0,1,2]]
  */
-Iterator.prototype.concatLeft = function(...iters){
-	const it = function*(){
-		for(let iter of iters) yield* iter;
+Iterator.prototype.concatLeft = function(...iters) {
+	const it = function*() {
+		for (let iter of iters) yield* iter;
 		yield* this.gen;
-	}
+	};
 	return new Iterator(it.call(this));
-}
+};
 
 /**
  * Merge multiple iterators
@@ -282,9 +281,9 @@ Iterator.prototype.concatLeft = function(...iters){
  * Iterator.fromArray([0,1,2])
  * .merge(Iterator.fromArray(["a","b","c"])) // [[0,"a",1,"b",2,"c"]]
  */
-Iterator.prototype.merge = function(...iters){
+Iterator.prototype.merge = function(...iters) {
 	return Iterator.fromMultiple(this.gen, ...iters);
-}
+};
 
 /**
  * Merge multiple iterators from left
@@ -295,9 +294,9 @@ Iterator.prototype.merge = function(...iters){
  * Iterator.fromArray([0,1,2])
  * .mergeLeft(Iterator.fromArray(["a","b","c"])) // [["a",0,"b",1,"c",2]]
  */
-Iterator.prototype.mergeLeft = function(...iters){
+Iterator.prototype.mergeLeft = function(...iters) {
 	return Iterator.fromMultiple(...iters, this.gen);
-}
+};
 
 /**
  * function which Iterator instance mepthod consumes
@@ -319,26 +318,26 @@ Iterator.prototype.mergeLeft = function(...iters){
  * Iterator.fromArray(["a","b","c","a","b","c"])
  * .accumulateWhile((c,x)=> x === "c") // [["a","b","c"],["a","b","c"]]
  */
-Iterator.prototype.accumulateWhile = function(fn, yieldRest = false){
-	const it = function* () {
+Iterator.prototype.accumulateWhile = function(fn, yieldRest = false) {
+	const it = function*() {
 		let step = this.gen.next();
 		let arr = [];
 		let i = 0;
-		while(!step.done){
+		while (!step.done) {
 			arr.push(step.value);
-			if(fn(arr, step.value, i)){
+			if (fn(arr, step.value, i)) {
 				yield arr;
 				arr = [];
 			}
 			step = this.gen.next();
 			i++;
-		};
-		if(arr.length > 0 && yieldRest){
+		}
+		if (arr.length > 0 && yieldRest) {
 			yield arr;
 		}
-	}
+	};
 	return new Iterator(it.call(this));
-}
+};
 
 /**
  * Accumulate multiple items of iterator into one
@@ -349,9 +348,9 @@ Iterator.prototype.accumulateWhile = function(fn, yieldRest = false){
  * @example
  * range(0,5).accumulateN(2, true) // [[0,1],[2,3],[4]]
  */
-Iterator.prototype.accumulateN = function(n, yieldRest = false){
-	return this.accumulateWhile((c,x,i) => c.length === n, yieldRest);
-}
+Iterator.prototype.accumulateN = function(n, yieldRest = false) {
+	return this.accumulateWhile((c, x, i) => c.length === n, yieldRest);
+};
 
 /**
  * Generator function
@@ -374,12 +373,12 @@ Iterator.prototype.accumulateN = function(n, yieldRest = false){
  * 	}
  * }) // ["a", "b", "c", "d"]
  */
-Iterator.prototype.subSplit = function(gen){
-	const it = function* () {
-		for(let item of this.gen) yield* gen(item);
-	}
+Iterator.prototype.subSplit = function(gen) {
+	const it = function*() {
+		for (let item of this.gen) yield* gen(item);
+	};
 	return new Iterator(it.call(this));
-}
+};
 
 /**
  * creates new Iterator from iterator
@@ -387,9 +386,9 @@ Iterator.prototype.subSplit = function(gen){
  * @param {iterator} iter - iterator
  * @returns {Iterator}
  */
-Iterator.new = function(iter){
+Iterator.new = function(iter) {
 	return new Iterator(iter);
-}
+};
 
 /**
  * Makes Iterator from array
@@ -397,12 +396,12 @@ Iterator.new = function(iter){
  * @param {Any[]} arr - array
  * @returns {Iterator}
  */
-Iterator.fromArray = function(arr){
-	const it = function* () {
-		for(let item of arr) yield item;
-	}
+Iterator.fromArray = function(arr) {
+	const it = function*() {
+		for (let item of arr) yield item;
+	};
 	return new Iterator(it.call(this));
-}
+};
 
 /**
  * Makes Iterator from array
@@ -415,22 +414,21 @@ Iterator.fromArray = function(arr){
  * 	Iterator.fromArray(["a","b","c"]),
  * ).toArray() // [1,"a","2","b",3,"c"]
  */
-Iterator.fromMultiple = function(...iters){
+Iterator.fromMultiple = function(...iters) {
 	const itersCopy = iters.slice(0);
-	const it = function* () {
-		while(itersCopy.length > 0){
-			for(let it of itersCopy){
+	const it = function*() {
+		while (itersCopy.length > 0) {
+			for (let it of itersCopy) {
 				let step = it.next();
-				if(!step.done){
+				if (!step.done) {
 					yield step.value;
 				} else {
 					itersCopy.splice(itersCopy.indexOf(it), 1);
-				};
+				}
 			}
 		}
-	}
+	};
 	return new Iterator(it.call(this));
-}
-
+};
 
 module.exports = Iterator;
